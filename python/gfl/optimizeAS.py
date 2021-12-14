2d4180292a443b229378320538c49e220c1d0f9d
Hangjian Li <hangjianli@Hangjians-MacBook-Pro.local>
2021-12-14 00:48:36 -0500

    reimplementation of gfl

---
+++ python/gfl/optimizeAS.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def block_optimizeAS(beta_init, weights, active_set, lambda):
+    """
+
+    Parameters:
+        beta_init : (a x p) matrix to initialize
+        active_set: (a x 1) vector with indeices of blocks in the active set
+
+
+    """
+
+
+
+
+    #compute vector S
+    XitX = i*(n - i)* di**2/n
+
+    
+    return 0
+    
+    
+
+
+class GFLasso:
+    """
+    A class to represent a person.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        first name of the person
+    surname : str
+        family name of the person
+    age : int
+        age of the person
+
+    Methods
+    -------
+    info(additional=""):
+        Prints the person's name and age.
+    """
+
+    def __init__(self, maxit=1e5, tol=1e-8):
+        self.maxit = maxit
+        self.tol = tol
+    
+    def block_coord_desc(self, Y, weights, lam):
+        """"""
+        self.n, self.p = Y.shape
+        
+        
+        beta = block_optimizeAS()
+        
+        self.reconstruct_signal()
+    
+    
+    def block_optimizeAS(self, beta_init, weights, active_set, lambda):
+        """
+        
+        Parameters:
+            beta_init : (a x p) matrix to initialize
+            active_set: (a x 1) vector with indeices of blocks in the active set
+        
+        
+        """
+        
+        
+        
+        
+        #compute vector S
+        XitX = i*(n - i)* di**2/n
+        
+    def reconstruct_signal(self):
+        pass
